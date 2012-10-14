32e0c5bb167a74e2adad2f74bc5a799917e187d7
BeetsRoots <beetrootbusiness@gmail.com>
2012-10-14 17:37:52 -0400

    color added

--- life.cpp
+++ life.cpp
@@ -44,6 +44,40 @@ char readmap(char map[][1000], int *mapwidth = 0, int *mapheight = 0){
 
 }
 
+void paint(string color, string word){
+
+    WORD RED = 0xC;
+    WORD YELLOW = 0xE;
+    WORD GREEN = 0xA;
+    WORD BLUE = 0x9;
+    
+    HANDLE hstdin  = GetStdHandle(STD_INPUT_HANDLE);
+	HANDLE hstdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// Remember how things were when we started
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(hstdout, &csbi);
+
+    if(color=="red"){
+        SetConsoleTextAttribute(hstdout, RED);
+    }else if(color=="yellow"){
+        SetConsoleTextAttribute(hstdout, YELLOW); 
+    }else if(color=="blue"){
+        SetConsoleTextAttribute(hstdout, BLUE); 
+    }else if(color=="green"){
+        SetConsoleTextAttribute(hstdout, GREEN); 
+    }
+
+    cout << word;
+    
+    FlushConsoleInputBuffer(hstdin);
+    
+	// Keep users happy
+	SetConsoleTextAttribute(hstdout, csbi.wAttributes);
+    
+}
+
+
 
 int main(){
 
